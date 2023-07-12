c5c0ffbfce39f0b146f1ccfd4c9442382ed53e88
MirkoPescio <mirkopes.4050@gmail.com>
2023-07-12 14:53:48 -0300

    Diseño-responsive_parte-2

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import './css/categoryItem.css'
+import React from "react";
+import "./css/categoryItem.css";
+import { Link } from "react-router-dom";
 
-
-const CategoryItem = ({item}) => {
+const CategoryItem = ({ item }) => {
   return (
-    <div className='categoryItem'>
-        <img className='categoryItem__image' src={item.img} alt={item.img} />
-        <div className='categoryItem__info'>
-            <h2 className='categoryItem__info__title'>{item.title}</h2>
-            <button className='categoryItem__info__button'>Ver</button>
-        </div>
+    <div className="categoryItem">
+      <img className="categoryItem__image" src={item.img} alt={item.img} />
+      <div className="categoryItem__info">
+        <h2 className="categoryItem__info__title">{item.title}</h2>
+        <Link to="/products">
+          <button className="categoryItem__info__button">Ver</button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CategoryItem
+export default CategoryItem;
